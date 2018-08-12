86ff6e6104c7daa3c1456144fb6ce0bd12c29dbe
Zach Miller <zach.miller@wustl.edu>
2018-08-11 21:09:40 -0500

    added models

--- RateMyDormProject/WebApp/admin.py
+++ RateMyDormProject/WebApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Dorm, Review
 
+
+#import models
+admin.site.register(Dorm)
+admin.site.register(Review)
--- RateMyDormProject/WebApp/models.py
+++ RateMyDormProject/WebApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+#table for dorms
+class Dorm(models.Model):
+	name = models.CharField(max_length=50, blank=False)
+	college = models.CharField(max_length=50, blank=False)
 
+
+#table for reviews
+class Review(models.Model):
+	dorm = models.ForeignKey(Dorm, on_delete=models.CASCADE)
+	description = models.CharField(max_length=200,null=False)
