1ca07e1834eb5cf314b21d0015bd08bf3c7acce4
Zach Miller <zach.miller@wustl.edu>
2018-08-11 20:52:55 -0500

    test

--- RateMyDormProject/WebApp/views.py
+++ RateMyDormProject/WebApp/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'WebApp/home.html')
 
+def about(request):
+	return render(request, 'WebApp/about.html')
+
 # Create your views here.
